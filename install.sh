a5977c7fd026ce9bdf61f0b07cb68fa4113bcbe8
Yosuke ONOUE <onoue@likr-lab.com>
2017-02-11 11:00:41 +0900

    add cookiecutterrc

--- install.sh
+++ install.sh
@@ -6,5 +6,6 @@ ln -F -n -s "$path/_vim" ~/.vim
 ln -F -s "$path/_hgrc" ~/.hgrc
 ln -F -s "$path/_gitconfig" ~/.gitconfig
 ln -F -s "$path/_gitignore" ~/.gitignore
+ln -F -s "$path/cookiecutterrc" ~/.cookiecutterrc
 ln -F -n -s "$path/nvim" ~/.config/nvim
 git clone https://github.com/Shougo/dein.vim.git ~/.cache/dein/repos/github.com/Shougo/dein.vim
